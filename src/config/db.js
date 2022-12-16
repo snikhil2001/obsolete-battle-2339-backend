aaab36a6799b3b63979ffe6949455a133b34c769
snikhil2001 <nikhilsahni2001@gmail.com>
2022-12-16 11:43:52 +0530

    added minor changes

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const connect = () => {
-  mongoose.set("strictQuery", false);
   return mongoose.connect(
     process.env.DB_URL,
     {
